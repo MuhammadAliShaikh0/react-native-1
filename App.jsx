cb4fdb6ac6d5cfc138304b3cd8be92e1a7b9395f
Muhammad Ali <159954209+MuhammadAliShaikh0@users.noreply.github.com>
2024-09-28 10:25:00 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { View, Text, Image } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:50,color:"red",textAlign:"center"}}>Hello React Native
+      
+      </Text>
+      
+      
+    </View>
+  )
+}
+
+export default App
